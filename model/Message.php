e29078cb397c58760978e42644cb123b9de33299
SNYdev <yanis.sahnoune@supinternet.fr>
2015-05-21 19:03:21 +0200

    mail function correction

--- model/Message.php
+++ model/Message.php
@@ -29,22 +29,54 @@ class Message {
     }
 }
 
-class Mail
+class pooMail
 {
-    protected $mail = 'snydesign75@gmail.com';
-    protected $subject = 'Transversale';
-    protected $message = 'Message';
-    protected $header = 'From:"De Coton à Soie" <snydesign75@gmail.com>';
-    protected $r = "\r\n";
+    // Mail du destinataire
+    public $mail = 'snydesign75@gmail.com';
+    public $subject = 'Transversale';
+    public $message = 'Message';
+    public $header = 'From:"De Coton à Soie" <snydesign75@gmail.com>';
+    public $r = "\r\n";
 
-    function __construct(){
+    public function subscriptionMail(){
+        // Titre du Mail
+        $this->subject = 'Votre inscription';
+        // Message du Mail
+        $this->message = 'Message pour votre inscription';
+        // Condition pour empecher l'envois auto
+        if (isset($_POST['nickname'])) {
+            $testMail = mail($this->mail, $this->subject, $this->message);
+                if ($testMail === true) {
+                    echo "Message envoyé";   
+                } else {
+                    echo "Probleme technique";
+                }
+        }   
+    }
+
+    public function newsletterMail(){
+        $this->subject = 'Votre newsletter';
+        $this->message = 'Message pour la newsletter';
+        if (isset($_POST['nickname'])) {
+            $testMail = mail($this->mail, $this->subject, $this->message);
+                if ($testMail === true) {
+                    echo "Message envoyé";
+                } else {
+                    echo "Probleme technique";
+                }
+        }
+    }
+
+    public function buyMail(){
+        $this->subject = 'Votre achat';
+        $this->message = 'Message pour votre achat';
         if (isset($_POST['nickname'])) {
             $testMail = mail($this->mail, $this->subject, $this->message);
             if ($testMail === true) {
-                echo "Message envoyé";   
+                echo "Message envoyé";
             } else {
                 echo "Probleme technique";
             }
-        }   
+        }
     }
 }
