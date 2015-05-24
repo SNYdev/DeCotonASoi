aeadb67e284d9e019a987aae2660f261c947b6a4
SNYdev <yanis.sahnoune@supinternet.fr>
2015-05-24 23:10:27 +0200

    add link

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport">
-    <title>Du coton a soie</title>
+    <title>De coton a soie</title>
     <link rel="stylesheet" type="text/css" href="vue/css/reset.css">
     <link rel="stylesheet" type="text/css" href="vue/css/style.css">
     <link rel="icon" type="image/png" href="vue/img/favicon.png"/>
@@ -91,9 +91,10 @@
 
     if (file_exists($cssController)) {
         echo '<link href="' . $cssController . '" type="text/css" rel="stylesheet">';
-    } else {
-        echo 'File is missing';
-    }
+    } 
+    // else {
+    //     echo 'File is missing';
+    // }
 
     if (file_exists($jsController)) {
         echo '<script language="javascript" src="' . $jsController . '">';
@@ -101,11 +102,10 @@
     }
 ?>
 </head>
-<body>
-    <div class="main">
-        <div class="header">
-            <img id="logosHeader" src="vue/img/LOGO-Projet-Transversale.svg" width="100px">
-        </div>
+<div class="main">
+    <div class="header">
+        <img id="logosHeader" src="vue/img/LOGO-Projet-Transversale.svg" width="100px">
+    </div>
         <div class="banner">
             <div class="txtBanner">
                 <h1 class="bannerTitle">De Coton à Soie</h1>
@@ -115,126 +115,18 @@
         <div class="nav">
             <ul class="navUlDcas">
                 <li class="navLiDcas"><a class="linkNav" href="?page=home">Accueil</a></li>
-                <li class="navLiDcas"><a class="linkNav" href="?page=stylist">Stylistes</a></li>
+                <li class="navLiDcas"><a class="linkNav" href="?page=inscription">Stylistes</a></li>
                 <?php
                     if (isset($_SESSION['user'])) {
-                        echo '<li class="navLiDcas"><a class="linkNav" href="#">Logout</a></li>';
-                        echo '<li class="navLiDcas"><a class="linkNav" href="#">Profil</a></li>';
+                        echo '<li class="navLiDcas"><a class="linkNav" href="?page=logout">Logout</a></li>';
+                        echo '<li class="navLiDcas"><a class="linkNav" href="?page=profil">Profil</a></li>';
                     } else {
-                        echo '<li class="navLiDcas"><a class="linkNav" href="#">Connexion</a></li>';
+                        echo '<li class="navLiDcas"><a class="linkNav" href="?page=login">Connexion</a></li>';
                     }
                 ?>
                 <li class="navLiDcas"><a class="linkNav" href="?page=home">Contact</a></li>
             </ul>
-        </div>
-        <div class="tutoWebsite">
-            <ul class="listTuto">
-                <li><img class="imgTuto" src="vue/img/tuto1.jpg"></li>
-                <li><img class="imgTuto" src="vue/img/tuto2.jpg"></li>
-                <li><img class="imgTuto" src="vue/img/tuto3.jpg"></li>
-                <li><img class="imgTuto" src="vue/img/tuto4.jpg"></li>
-            </ul>
-        </div>
-        <div class="stylist">
-            <div class="txtStylist">
-                <h1 class="titleStylist">Nos stylistes</h1>
-                <h2 class="subTitleStylist">Meet the team</h2>
-                <p class="areaTxt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit, arcu et tempus aliquam, felis massa semper mi, at vestibulum nisl lorem a sem. Donec libero sem, tincidunt sed feugiat eget, lobortis vitae erat. Aliquam pulvinar velit vel feugiat tempor. Etiam diam risus, eleifend sed ornare sit amet, cursus nec orci. Suspendisse sollicitudin leo vitae quam feugiat pharetra. Pellentesque interdum, </p>
-            </div>
-            <div class="imgStylist">
-                <div class="carre" id="img1">
-                    <img id="allStylist" src="vue/img/img-stylist.png">
-                </div>
-            </div>
-        </div>
-        <div class="information">
-            <div class="imgInformation">
-                <div class="firstLineImg">
-                    <img src="vue/img/maeva-gabriel.jpg" width="150px">
-                    <img src="vue/img/hugo-seguin.jpg" width="150px">
-                </div>
-                <div class="secondLineImg" width="150px">
-                    <img src="vue/img/jeremy-deluze.jpg" width="150px" height='150px'>
-                    <img src="vue/img/mathieu-phung.jpg" width="150px">
-                    <img src="vue/img/yanis-sahnoune.jpg" width="150px">
-                </div>
-                <div class="thirdLineImg" width="150px">
-                    <img src="vue/img/florent-laize.jpg" width="150px">
-                    <img src="vue/img/paul-vernaz.jpg" width="150px">
-                </div>
-            </div>
-            <div class="txtInformation">
-                <h1 class="titleInformation">Qui somme-nous ?</h1>
-                <h2 class="subTitleInformation">Spécialiste dans le domaine de lingerie personnalisée.</h2>
-                <p class="areaTxtInformation">Nous mettons à votre disposition une équipe de styliste avec des styles divers et variés :
-                Choisissez le styliste qui colle le plus à votre style, il vous aidera à concevoir votre lingerie.<br>
-                Rentrez vos mesures, votre lingerie vous ira comme un gant.<br>
-                Personnalisez votre lingerie, elle sera unique au monde</p>
-            </div>
-        </div>
-        <div class="membership">
-        </div>
-        <div class="begin">
-            <div class="contentBegin">
-                <div class="txtBegin">
-                    <h1 class="titleBegin">Pret pour commencer l'aventure ?</h1>
-                    <h2 class="subTitleBegin">Pour créer votre lingerie, laissez vous guider par nos etapes</h2>
-                </div>
-                <input type="button" class="buttonBegin" value="A toi de jouer !">
-            </div>
-        </div>
-        <div class="footer">
-            <div class="mainFooter">
-                <div class="iconeDCAS">
-                    <div class="iconeIMG">
-                        <img src="vue/img/logo-payement.png" width='70px'>
-                        <p class="iconeTxt" id="Psecurity">Paiement securisé</p>
-                    </div>
-                    <div class="iconeIMG">
-                        <img src="vue/img/logo-livraison.png" width='70px'>
-                        <p class="iconeTxt" id="Pexpedition">Paiement à l'expedition</p>
-                    </div>
-                    <div class="iconeIMG">
-                        <img src="vue/img/logo-expedition.png" width='90px'>
-                        <p class="iconeTxt" id="Poffer">Livraison offerte</p>
-                    </div>
-                    <div class="iconeIMG">
-                        <img src="vue/img/logo-changement.png" width='50px'>
-                        <p class="iconeTxt" id="Pdays">30 jours pour changer d'avis</p>
-                    </div>
-                </div>
-                <div id="barre">
-                </div>
-                <div class="moreInformation">
-                    <div class="contactInformation">
-                        <h1 class="h1Information">Ma commande<br></h1>
-                        <ul class="liInformation">
-                            <li><a class="linkFooter" href="">Suivi de commande</a></li>
-                            <li><a class="linkFooter" href="">Frais d'envoie</a></li>
-                            <li><a class="linkFooter" href="">Délai de livraison</a></li>
-                            <li><a class="linkFooter" href="">Echange et remboursement</a></li>
-                            <li><a class="linkFooter" href="">Moyen de paiement</a></li>
-                        </ul>
-                    </div>
-                    <div class="contact">
-                        <h1 class="h1Information">Nous contacter</h1>
-                        <ul class="liInformation">
-                            <li>06.28.42.93.59</li>
-                            <li>maeva.gabriel96@gmail.com</li>
-                        </ul>
-                    </div>
-                    <div class="socialNetwork">
-                        <h1 class="h1Information" id="followBlock">Suivez-Nous</h1>
-                        <div class="contactIcone">
-                            <img src="vue/img/facebook-icon-flat.png" width="40px">
-                            <img src="vue/img/logo-Twitter.png" width="40px">
-                            <img src="vue/img/logo-insta.jpg" width="40px">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>              
+        </div> 
 <?php
 
 
@@ -257,7 +149,7 @@
         echo 'File is missing';
     }
 ?>
-<script type="text/javascript">
-</script>
+    <script type="text/javascript">
+    </script>
 </body>
 </html>
