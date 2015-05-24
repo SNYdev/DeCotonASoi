4e376b4d82896b7253900073053b86947bc66c1e
GxBot <jeremy.deluze@supinternet.fr>
2015-05-24 19:24:19 +0200

    other pages

--- vue/subscription.php
+++ vue/subscription.php
@@ -1,120 +1,199 @@
-<form action="" method="post">
-    <p>Civilité : <input type="radio" name="gender" value="1" checked/>Madame <input type="radio" name="gender" value="2"/>Monsieur</p>
-    <p>*Prénom : <input type="text" name="firstname" id="firstname"/></p><p id="error1"></p>
-    <p>*Nom : <input type="text" name="lastname" id="lastname" /></p><p id="error2"></p>
-    <p>*Pseudo : <input type="text" name="login" id="login" /></p><p id="error11"></p>
-    <p>Mensuration : <input type="text" name="size" id="size" /></p><p id="error3"></p>
-    <p>*Adresse : <input type="text" name="address" id="address" /></p><p id="error4"></p>
-    <p>*Ville : <input type="text" name="city" id="city" /></p><p id="error5"></p>
-    <p>*Code postal : <input type="text" name="zcode" id="zcode" /></p><p id="error6"></p>
-    <p>*Email : <input type="text" name="email" id="email" /></p><p id="error10"></p>
-    <p>Téléphone : <input type="tel" name="tel" id="tel" /></p><p id="error7"></p>
-    <p>*Mot de passe : <input type="password" name="password" id="password" /></p><p id="error8"></p>
-    <p>*Vérificaion mot de passe : <input type="password" name="password_check" id="password_check" /></p><p id="error9"></p>
-    <p>Newsletter : <input type="checkbox" name="newsletter" />Je souhaite recevoir les newsletter Du coton à soie</p>
-    <p><input type="submit" /></p>
-</form>
-
-<script>
-    $(function() {
-        $("#firstname").change(function () {
-            if ($(this).val().length < 2) {
-                $("#error1").html("Prénom trop court");
-            }
-            else {
-                $("#error1").html("")
-            }
-        });
-
-        $("#lastname").change(function () {
-            if ($("#lastname").val().length < 2) {
-                $("#error2").html("Nom trop court");
-            }
-            else {
-                $("#error2").html("")
-            }
-        });
-
-        $("#login").change(function () {
-            if ($(this).val().length < 2) {
-                $("#error11").html("Pseudo trop court");
-            }
-            else {
-                $("#error11").html("")
-            }
-        });
-
-        $("#city").change(function () {
-            if ($("#city").val().length < 3) {
-                $("#error5").html("Ville trop courte");
-            }
-            else {
-                $("#error5").html("")
-            }
-        });
-
-        $("#zcode").change(function () {
-            if ($(this).val().length != 5 || isNaN($(this).val())) {
-                $("#error6").html("Le code postal doit être de 5 chiffres");
-            }
-            else {
-                $("#error6").html("")
-            }
-        });
-
-        var reg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-        $("#email").change(function () {
-            if (!(reg.test($(this).val()))) {
-                $("#error10").html("email invalide");
-            }
-            else {
-                $("#error10").html("")
-            }
-        });
-
-        $("#password").change(function () {
-            if ($(this).val().length < 6) {
-                $("#error8").html("Le mot de passe doit contenir au minimum 6 caracères");
-            }
-            else {
-                $("#error8").html("")
-            }
-        });
-
-        $("#password_check").change(function () {
-            if ($(this).val() !== $("#password").val()) {
-                $("#error9").html("Les 2 mots de passes ne correspondent pas");
-            }
-            else {
-                $("#error9").html("")
-            }
-        });
-    });
-
-</script>
-
-<?php
-if(!empty($_POST['firstname'])  && !empty($_POST['lastname']) && !empty($_POST['login']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['zcode']) && !empty($_POST['password']) && !empty($_POST['password_check']) && !empty($_POST['email']) && (strlen($_POST['firstname']) >= 2) && (strlen($_POST['lastname']) >= 2) && (strlen($_POST['login']) >= 2) && (strlen($_POST['city']) >= 3) && (strlen($_POST['zcode']) == 5) && is_numeric($_POST['zcode']) && (strlen($_POST['password']) >= 6) && ($_POST['password_check'] === $_POST['password'])) {
-    $count = $user->countUserByName($_POST['login']);
-
-    if($count['nb_user']) {
-        $user->addMessageFlash('info','Login existe déjà.');
-    }
-    else {
-        $user->addUser($_POST['gender'], $_POST['firstname'], $_POST['lastname'], $_POST['login'], $_POST['password'], $_POST['address'],$_POST['city'], $_POST['zcode'], $_POST['email']);
-        $info = $user->getUserByName($_POST['firstname']);
-
-        $_SESSION['user'] = ['id' => $info['id'], 'name' => $info['login']];
-
-        $user->addMessageFlash('success','Inscription réussie !');
-
-        header('Location: http://localhost/DeCotonASoi/');
-
-        exit;
-    }
-}
-else {
-    $user->addMessageFlash('info','Le formulaire a été mal saisi.');
-}
-
-?>
+    <br><br><br><br>
+    
+    <form>
+        <fieldset>
+            <legend>VOS INFORMATIONS PERSONELLES</legend>
+            <br>
+            <input type="radio" name="sex" value="Mme" checked>Mme
+            <br>
+            <input type="radio" name="sex" value="Mr">Mr
+            <br>
+            Prenom<br>
+            <input type="text" name="Prenom">
+            <br>
+            Nom<br>
+            <input type="text" name="Nom">
+            <br>
+            Adresse E-mail<br>
+            <input type="text" name="mail">
+            <br>
+            Mot de passe<br>
+            <input type="text" name="mdp">
+            <br>
+            Confirmation du mot de passe<br>
+            <input type="text" name="check">
+            <br>
+            Date de naissance<br>
+             <select name="jours">
+                 <option value="1">1</option>
+                 <option value="2">2</option>
+                 <option value="3">3</option>
+                 <option value="4">4</option>
+                 <option value="5">5</option>
+                 <option value="6">6</option>
+                 <option value="7">7</option>
+                 <option value="8">8</option>
+                 <option value="9">9</option>
+                 <option value="10">10</option>
+                 <option value="11">11</option>
+                 <option value="12">12</option>
+                 <option value="13">13</option>
+                 <option value="14">14</option>
+                 <option value="15">15</option>
+                 <option value="16">16</option>
+                 <option value="17">17</option>
+                 <option value="18">18</option>
+                 <option value="19">19</option>
+                 <option value="20">20</option>
+                 <option value="21">21</option>
+                 <option value="22">22</option>
+                 <option value="23">23</option>
+                 <option value="24">24</option>
+                 <option value="25">25</option>
+                 <option value="26">26</option>
+                 <option value="27">27</option>
+                 <option value="28">28</option>
+                 <option value="29">29</option>
+                 <option value="30">30</option>
+                 <option value="31">31</option>
+            </select>
+            <select name="mois">
+                 <option value="1">1</option>
+                 <option value="2">2</option>
+                 <option value="3">3</option>
+                 <option value="4">4</option>
+                 <option value="5">5</option>
+                 <option value="6">6</option>
+                 <option value="7">7</option>
+                 <option value="8">8</option>
+                 <option value="9">9</option>
+                 <option value="10">10</option>
+                 <option value="11">11</option>
+                 <option value="12">12</option>
+            </select>
+            <select name="année">
+                 <option value="2015">2015</option>
+                 <option value="2014">2014</option>
+                 <option value="2013">2013</option>
+                 <option value="2012">2012</option>
+                 <option value="2011">2011</option>
+                 <option value="2010">2010</option>
+                 <option value="2009">2009</option>
+                 <option value="2008">2008</option>
+                 <option value="2007">2007</option>
+                 <option value="2006">2006</option>
+                 <option value="2005">2005</option>
+                 <option value="2004">2004</option>
+                 <option value="2003">2003</option>
+                 <option value="2002">2002</option>
+                 <option value="2001">2001</option>
+                 <option value="2000">2000</option>
+                 <option value="1999">1999</option>
+                 <option value="1998">1998</option>
+                 <option value="1997">1997</option>
+                 <option value="1996">1996</option>
+                 <option value="1995">1995</option>
+                 <option value="1994">1994</option>
+                 <option value="1993">1993</option>
+                 <option value="1992">1992</option>
+                 <option value="1991">1991</option>
+                 <option value="1990">1990</option>
+                 <option value="1989">1989</option>
+                 <option value="1988">1988</option>
+                 <option value="1987">1987</option>
+                 <option value="1986">1986</option>
+                 <option value="1985">1985</option>
+                 <option value="1984">1984</option>
+                 <option value="1983">1983</option>
+                 <option value="1982">1982</option>
+                 <option value="1981">1981</option>
+                 <option value="1980">1980</option>
+                 <option value="1979">1979</option>
+                 <option value="1978">1978</option>
+                 <option value="1977">1977</option>
+                 <option value="1976">1976</option>
+                 <option value="1975">1975</option>
+                 <option value="1974">1974</option>
+                 <option value="1973">1973</option>
+                 <option value="1972">1972</option>
+                 <option value="1971">1971</option>
+                 <option value="1970">1970</option>
+                 <option value="1969">1969</option>
+                 <option value="1968">1968</option>
+                 <option value="1967">1967</option>
+                 <option value="1966">1966</option>
+                 <option value="1965">1965</option>
+                 <option value="1964">1964</option>
+                 <option value="1963">1963</option>
+                 <option value="1962">1962</option>
+                 <option value="1961">1961</option>
+                 <option value="1960">1960</option>
+                 <option value="1959">1959</option>
+                 <option value="1958">1958</option>
+                 <option value="1957">1957</option>
+                 <option value="1956">1956</option>
+                 <option value="1955">1955</option>
+                 <option value="1954">1954</option>
+                 <option value="1953">1953</option>
+                 <option value="1952">1952</option>
+                 <option value="1951">1951</option>
+                 <option value="1950">1950</option>
+                 <option value="1949">1949</option>
+                 <option value="1948">1948</option>
+                 <option value="1947">1947</option>
+                 <option value="1946">1946</option>
+                 <option value="1945">1945</option>
+                 <option value="1944">1944</option>
+                 <option value="1943">1943</option>
+                 <option value="1942">1942</option>
+                 <option value="1941">1941</option>
+                 <option value="1940">1940</option>
+                 <option value="1939">1939</option>
+                 <option value="1938">1938</option>
+                 <option value="1937">1937</option>
+                 <option value="1936">1936</option>
+                 <option value="1935">1935</option>
+                 <option value="1934">1934</option>
+                 <option value="1933">1933</option>
+                 <option value="1932">1932</option>
+                 <option value="1931">1931</option>
+                 <option value="1930">1930</option>
+                 <option value="1929">1929</option>
+                 <option value="1928">1928</option>
+                 <option value="1927">1927</option>
+                 <option value="1926">1926</option>
+                 <option value="1925">1925</option>
+                 <option value="1924">1924</option>
+                 <option value="1923">1923</option>
+                 <option value="1922">1922</option>
+                 <option value="1921">1921</option>
+                 <option value="1920">1920</option>
+                 <option value="1919">1919</option>
+                 <option value="1918">1918</option>
+                 <option value="1917">1917</option>
+                 <option value="1916">1916</option>
+                 <option value="1915">1915</option>
+                 <option value="1914">1914</option>
+                 <option value="1913">1913</option>
+                 <option value="1912">1912</option>
+                 <option value="1911">1911</option>
+                 <option value="1910">1910</option>
+                 <option value="1909">1909</option>
+                 <option value="1908">1908</option>
+                 <option value="1907">1907</option>
+                 <option value="1906">1906</option>
+                 <option value="1905">1905</option>
+                 <option value="1904">1904</option>
+                 <option value="1903">1903</option>
+                 <option value="1902">1902</option>
+                 <option value="1901">1901</option>
+                 <option value="1900">1900</option>
+            </select><br>
+            Newsletter<input type="checkbox" name="Newsletter" value="Newsletter"><br>
+            <br>
+            <p>Soyez informée des offres Du Conton à soie et recevez une invitation aux ventes privées du site.</p>
+            <br><br><br>
+            <input type="submit" value="Submit"></fieldset>
+    </form>
+    
+    <br><br><br><br>
