49c695a6046a652654ea3a5d6d825212107478ee
SNYdev <yanis.sahnoune@supinternet.fr>
2015-05-28 01:22:29 +0200

    add css stylist.php

--- vue/stylistes1.php
+++ vue/stylistes1.php
@@ -3,21 +3,35 @@
         <span class="Marg">___</span>
         <h2 class="subTitleStylist1">Nous avons choisi de nous entourer de stylistes spécialisés chacun dans l'une de ces categories afin de répondre au mieux à la demande de notre clientèle, quelle qu'elle soit.</h2>
     </div>
-    
-    
-    
-    <div>
-        <img class="face" alt="MARINE ANTON" src="vue/img/Marine-Anton.png">
-        <h4>MARINE ANTON</h4>
-        
-        <h5><span class="black">_</span> Loungwear</h5>
-        
-        <p>Marine Anton, anciennement styliste pour un magazine féminin, a ensuite collaboré avec Claire Judge qui a lancé une marque de loungewear en 2013. Elle sait qu’avoir à sa disposition une lingerieconfortable pour cocooner chez soi ne dispense pas qu’elle soit raffinée et élégante.</p>
-        
-        <img class="face-img" alt="MARINE ANTON 1" src="vue/img/loungewear-1.jpg">
-        <img class="face-img" alt="MARINE ANTON 2" src="vue/img/loungewear-2.jpg">
-        <img class="face-img" alt="MARINE ANTON 3" src="vue/img/loungewear-3.jpg">
+
+    <div class="blockStylist">
+        <div class="blockStylistContent">
+            <form method="post">
+            <div class="blockSname">Marine Anton</div>
+            <div class="blockStype">Loungewear</div>
+            <div class="blockSresum">Marine Anton, anciennement styliste pour un magazine féminin, a ensuite collaboré avec Claire Judge qui a lancé une marque de loungewear en 2013. Elle sait qu’avoir à sa disposition une lingerieconfortable pour cocooner chez soi ne dispense pas qu’elle soit raffinée et élégante.</div>
+            <input type="submit" class="blockSsubmit" value="Choisissez-moi">
+            </form>
+        </div>
+        <div class="blockSpictures">
+            <img src="vue/img/loungewear-1.jpg" alt="" class="pictures" id="img1" width="100px">
+            <img src="vue/img/loungewear-2.jpg" alt="" class="pictures" id="img2" width="100px">
+            <img src="vue/img/loungewear-3.jpg" alt="" class="pictures" id="img3" width="100px">
+        </div>
     </div>
+    
+<!--         <div class="">
+            <img class="face" alt="MARINE ANTON" src="vue/img/Marine-Anton.png">
+            <h4>MARINE ANTON</h4>
+            
+            <h5><span class="black">_</span> Loungwear</h5>
+            
+            <p>Marine Anton, anciennement styliste pour un magazine féminin, a ensuite collaboré avec Claire Judge qui a lancé une marque de loungewear en 2013. Elle sait qu’avoir à sa disposition une lingerieconfortable pour cocooner chez soi ne dispense pas qu’elle soit raffinée et élégante.</p>
+            
+            <img class="face-img" alt="MARINE ANTON 1" src="vue/img/loungewear-1.jpg">
+            <img class="face-img" alt="MARINE ANTON 2" src="vue/img/loungewear-2.jpg">
+            <img class="face-img" alt="MARINE ANTON 3" src="vue/img/loungewear-3.jpg">
+        </div>
     
     
     
@@ -77,4 +91,4 @@
         <img class="face-img" alt="MARINE ANTON 1" src="vue/img/loungewear-1.jpg">
         <img class="face-img" alt="MARINE ANTON 2" src="vue/img/loungewear-2.jpg">
         <img class="face-img" alt="MARINE ANTON 3" src="vue/img/loungewear-3.jpg">
-    </div>
+    </div> -->
